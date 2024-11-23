0c8cf369031bb7f82b34c7415042ace0d34eeb4f
Taombawkry <thomas.gondwe@outlook.com>
2024-11-23 05:18:29 +0200

    `useSearchParams ` to window search

--- packages/dapp/src/app/(main)/connect-wallet/page.tsx
+++ packages/dapp/src/app/(main)/connect-wallet/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 import React, { useEffect } from 'react';
 import { useAccount } from 'wagmi';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import ConnectWalletPrompt from '@/utils/methods/ticketPurchase/connectModal';
 
 export default function ConnectWallet() {
    const { address } = useAccount(); // Get the connected wallet address
    const router = useRouter();
+   const searchParams = useSearchParams(); // Access query parameters
 
    // Get the redirect URL from query parameters
-   const redirectUrl = new URLSearchParams(window.location.search).get('redirect') || '/';
+   const redirectUrl = searchParams.get('redirect') || '/';
 
    useEffect(() => {
       // If the wallet is connected, redirect to the original page
