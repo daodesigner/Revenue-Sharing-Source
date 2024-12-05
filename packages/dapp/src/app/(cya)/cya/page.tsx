47ec4256f6e9da2255bc54cbc49c01fbdf251024
Taombawkry <thomas.gondwe@outlook.com>
2024-12-05 03:45:26 +0200

    ref update

--- packages/dapp/src/app/(cya)/cya/page.tsx
+++ packages/dapp/src/app/(cya)/cya/page.tsx
@@ -189,7 +189,6 @@ export default function Cya() {
                      size={'medium'}
                      onClick={() => !timeLeft && buttonConfig.action}
                   >
-                     Purchase
                   </Button>
                )}
             </div>
