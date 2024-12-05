5975c328ce5ef9cdd0bd2f21890713807c785fde
Taombawkry <thomas.gondwe@outlook.com>
2024-12-05 03:21:27 +0200

    updated CTA

--- packages/dapp/src/app/components/heroSection.tsx
+++ packages/dapp/src/app/components/heroSection.tsx
@@ -19,7 +19,7 @@ function HeroSection() {
             </div>
             <div className="flex gap-2">
                <Link href={'/cya'}>
-                  <Button>Buy Ticket</Button>
+                  <Button>Purchase Ticket</Button>
                </Link>
                <Link href={'/distribution'}>
                   <Button variant="white">Learn more</Button>
